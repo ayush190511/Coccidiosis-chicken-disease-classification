bc9224e71cae9b70ee083a889a1e88c1d434c16b
Ayush <ayush642001@gmail.com>
2026-01-27 01:49:51 +0530

    prepare base model completed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -10,4 +12,17 @@ try:
     logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx================x")
 except Exception as e:
     logger.exception(f"Error occurred in stage {STAGE_NAME} : {e}")
+    raise e
+
+
+
+STAGE_NAME = "Prepare Base Model Stage"
+try:
+    logger.info(f"***************************************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
     raise e
--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -7,3 +7,15 @@ class DataIngestionConfig:
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path                   # Type hint: This expects a Path object
+    base_model_path: Path            # Where the generic model (e.g., VGG16) is saved
+    updated_base_model_path: Path    # Where your custom model will be saved
+    params_image_size: list          # [224, 224, 3]
+    params_learning_rate: float      # 0.001
+    params_include_top: bool         # False (transfer learning)
+    params_weights: str              # 'imagenet'
+    params_classes: int  
