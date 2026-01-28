0e714d1c38b6bb247393fa305c420211ec726f80
Ayush <ayush642001@gmail.com>
2026-01-28 21:24:14 +0530

    prepare callbacks added

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -19,3 +19,10 @@ class PrepareBaseModelConfig:
     params_include_top: bool         # False (transfer learning)
     params_weights: str              # 'imagenet'
     params_classes: int  
+
+
+@dataclass(frozen=True)
+class PrepareCallbacksConfig:
+    root_dir: Path
+    tensorboard_root_log_dir: Path
+    checkpoint_model_filepath: Path
